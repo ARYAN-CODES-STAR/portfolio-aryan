ad0422cebf7fd489b57f6514c1a472b7f048338e
ARYAN-CODES-STAR <aryansh0004@gmail.com>
2024-03-14 23:42:50 +0530

    <UI> <Home>Add Homepage and configuration changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    loader: "default",
+    path: "/_next/image",
+  },
+};
 
 export default nextConfig;
