59530d4826aafdd0e202f50d2f34e6c6139b5839
ARYAN-CODES-STAR <aryansh0004@gmail.com>
2024-03-15 23:01:27 +0530

    <UI> <Footer> Added Footer Page

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import HomePage from "@/components/HomePage"
 import Projects from "@/components/Projects";
 import Skills from "@/components/Skills";
 import Contact from "@/components/ContactForm";
+import Footer from "@/components/Footer"
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
       <Projects />
       <Skills />
       <Contact />
+      <Footer />
     </div>
   );
 }
